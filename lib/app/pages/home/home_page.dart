bc2e7eff5b91dc27b069a4837874e98a1c1c86ab
Lucas Carlos <luc.cjov@rede.ulbra.br>
2022-04-19 20:20:21 -0300

    :art: Work in progress create appbar of home page
    create appbar of home page

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -6,8 +6,22 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
-        title: const Text('Home Page'),
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        elevation: 1,
+        title: Image.network(
+          'https://servicos.ulbra.br/login/57723f29cf333fdb62cd6f9459bb4f7c.png',
+          height: 50,
+        ),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+        ),
       ),
       body: Container(),
     );
